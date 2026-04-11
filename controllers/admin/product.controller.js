f37ee85b6989707eda8ef238eb501d3ae6e9c948
Tien Thanh Dang <168884195+dangtienvn@users.noreply.github.com>
2026-04-11 07:50:05 +0700

    fix: pagination feature and upload feature interface

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -29,23 +29,6 @@ module.exports.index = async (req, res) => {
     // End of search
 
     // Pagination
-    let objectPagination = {
-        currentPage: 1,
-        limitItem: 4
-    };
-
-    if (req.query.page) {
-        const p = parseInt(req.query.page);
-        if (!isNaN(p) && p > 0) objectPagination.currentPage = p;
-    }
-
-    if (req.query.limit) {
-        const l = parseInt(req.query.limit);
-        if (!isNaN(l) && l > 0) objectPagination.limitItem = l;
-    }
-
-    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
-
     const countProducts = await Product.countDocuments(find);
     const objectPagination = paginationHelper(req.query, countProducts, { 
         limitDefault: 4, 
@@ -57,22 +40,6 @@ module.exports.index = async (req, res) => {
         .limit(objectPagination.limitItem)
         .skip(objectPagination.skip);
 
-    // Build pagination pages window (max 5 pages visible)
-    const windowSize = 5;
-    let start = Math.max(1, objectPagination.currentPage - Math.floor(windowSize / 2));
-    let end = Math.min(objectPagination.totalPage, start + windowSize - 1);
-    if (end - start + 1 < windowSize) {
-        start = Math.max(1, end - windowSize + 1);
-    }
-    objectPagination.pages = [];
-    for (let i = start; i <= end; i++) objectPagination.pages.push(i);
-
-    objectPagination.hasPrev = objectPagination.currentPage > 1;
-    objectPagination.hasNext = objectPagination.currentPage < objectPagination.totalPage;
-    objectPagination.prevPage = objectPagination.hasPrev ? objectPagination.currentPage - 1 : null;
-    objectPagination.nextPage = objectPagination.hasNext ? objectPagination.currentPage + 1 : null;
-    objectPagination.firstPage = 1;
-    objectPagination.lastPage = objectPagination.totalPage;
 
     // Build base query string excluding page param so view can append `page=` easily
     const queryObj = { ...req.query };
