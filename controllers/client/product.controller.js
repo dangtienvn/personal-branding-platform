81d1b62b329ea1cb0ae1ca1e6d0d397d6f60694b
Tien Thanh Dang <168884195+dangtienvn@users.noreply.github.com>
2026-04-13 21:20:29 +0700

    feat: add a feature to display product in the client panel

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -2,10 +2,13 @@ const Product = require("../../models/product.model");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
-  const products = await Product.find({});
+  const products = await Product.find({
+    status: "active",
+    deleted: false
+  });
 
   const newProducts = products.map(item => {
-    item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
+    item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0);
     return item;
   });
 
@@ -13,4 +16,31 @@ module.exports.index = async (req, res) => {
     pageTitle: "Danh sách sản phẩm",
     products: newProducts
   });
+};
+
+// [GET] /products/detail/:id
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      _id: req.params.id,
+      status: "active"
+    };
+
+    const product = await Product.findOne(find);
+
+    if (product) {
+      product.priceNew = (product.price * (100 - product.discountPercentage) / 100).toFixed(0);
+
+      res.render("client/pages/products/detail", {
+        pageTitle: product.title,
+        product: product
+      });
+    } else {
+      res.redirect("/products");
+    }
+
+  } catch (error) {
+    res.redirect("/products");
+  }
 };
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/client/product.controller");
 
-// Trang sản phẩm
 router.get("/", controller.index);
+
+router.get("/detail/:id", controller.detail);
 
 module.exports = router;
